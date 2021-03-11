d3e518a3de0bd6b521c07cdc1b6fe42d4eb749d6
Gideon Paul <gidzpaul95@gmail.com>
2021-03-11 07:17:09 -0800

    Skeleton methods for notes service.

--- src/main/java/com/gideon/scribe/notes/service/NotesService.java
+++ src/main/java/com/gideon/scribe/notes/service/NotesService.java
@@ -1,5 +1,15 @@
 package com.gideon.scribe.notes.service;
 
-public interface NotesService {
+import com.gideon.scribe.notes.models.Note;
 
+import java.util.List;
+
+public interface NotesService {
+    public Note save(Note note);
+
+    public Note findById(Integer noteId);
+
+    public List<Note> findAll();
+
+    public void deleteNote(Integer noteId);
 }
--- src/main/java/com/gideon/scribe/notes/service/NotesServiceImpl.java
+++ src/main/java/com/gideon/scribe/notes/service/NotesServiceImpl.java
@@ -1,7 +1,29 @@
 package com.gideon.scribe.notes.service;
 
+import com.gideon.scribe.notes.models.Note;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NotesServiceImpl implements NotesService {
+    @Override
+    public Note save(Note note) {
+        return null;
+    }
+
+    @Override
+    public Note findById(Integer noteId) {
+        return null;
+    }
+
+    @Override
+    public List<Note> findAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteNote(Integer noteId) {
+
+    }
 }
