cbc83cfa11ad1b4d88f683887f93e0e16bf545cf
Gideon Paul <gidzpaul95@gmail.com>
2021-03-11 07:05:42 -0800

    Added url pattern that notes controller will be handling.

--- src/main/java/com/gideon/scribe/notes/controller/NotesController.java
+++ src/main/java/com/gideon/scribe/notes/controller/NotesController.java
@@ -1,7 +1,9 @@
 package com.gideon.scribe.notes.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/notes")
 public class NotesController {
 }
