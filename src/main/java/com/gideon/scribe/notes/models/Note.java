2e7e58745e1021237d6ed1881de15baebb8d5374
Gideon Paul <gidzpaul95@gmail.com>
2021-03-11 07:35:33 -0800

    Changed Note to a javax persistence Entity.

--- src/main/java/com/gideon/scribe/notes/models/Note.java
+++ src/main/java/com/gideon/scribe/notes/models/Note.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Integer id;
     String title;
     String content;
